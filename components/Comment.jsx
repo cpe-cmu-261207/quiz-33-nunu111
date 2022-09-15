ed9ea7965535b077c877df0509e1f83302fdc526
nunu111 <nunu111za@hotmail.com>
2022-09-15 14:38:54 +0700

    q1 t1

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -4,10 +4,40 @@ import Reply from "./Reply";
 export default function Comment(props) {
   return (
     <div>
-      {/* Entire Comment div */}
-      <div></div>
-
-      {/*render Reply here... */}
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.userpic}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {props.username}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{props.usercontext}</span>
+          <div className="d-flex align-items-center gap-1">
+            {props.userlike > 0 ? <img src="/like.svg" width={20}></img> : null}
+            <span style={{ color: "#B0B3B8" }}>
+              {props.userlike > 0 ? <span>{props.userlike} คน</span> : null}
+            </span>
+          </div>
+        </div>
+      </div>
+      {props.userreply.map((replysec, i) => (
+        <Reply
+          key={i}
+          username={replysec.username}
+          userpic={replysec.userImagePath}
+          usercontext={replysec.replyText}
+          userlike={replysec.likeNum}
+        />
+      ))}
     </div>
   );
 }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    <div className="d-flex gap-2 my-2 ps-5">
+      <img
+        src={props.userpic}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="rounded rounded-3 p-2"
+        style={{ backgroundColor: "#3A3B3C" }}
+      >
+        <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+          {props.username}
+        </span>
+        <br />
+        <span style={{ color: "#E4E6EB" }}>{props.usercontext}</span>
+
+        <div className="d-flex align-items-center gap-1">
+          {props.userlike > 0 ? <img src="/like.svg" width={20}></img> : null}
+          <span style={{ color: "#B0B3B8" }}>
+            {props.userlike > 0 ? <span>{props.userlike} คน</span> : null}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
